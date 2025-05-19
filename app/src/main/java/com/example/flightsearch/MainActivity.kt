cf8e311acc4e79795fba2aff30395f14c3e1c485
timurkkkk <ytimurik@gmail.com>
2025-05-19 16:01:39 +0400

    feat: create flight search app
    created: searchbar, flights list from chosen airport, favorite flights list

---
+++ app/src/main/java/com/example/flightsearch/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.flightsearch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.flightsearch.ui.FlightSearchApp
+import com.example.flightsearch.ui.theme.FlightSearchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val appContainer = (application as FlightSearchApplication).container
+        
+        setContent {
+            FlightSearchTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FlightSearchApp(appContainer)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/flightsearch/data/model/Flight.kt
@@ -0,0 +1,8 @@
+package com.example.flightsearch.data.model
+
+data class Flight(
+    val departureCode: String,
+    val departureName: String,
+    val destinationCode: String,
+    val destinationName: String
+)
